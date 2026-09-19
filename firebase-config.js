574ad580ef1df98c372144fd059eb9db575ea86a
sherwin19git <113541998+sherwin19git@users.noreply.github.com>
2026-09-19 09:46:01 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  "apiKey": "AIzaSyDmkuV03bg1x_zMGF6bFoXQVm2Ha8Lh2Ro",
+  "authDomain": "gabssari.firebaseapp.com",
+  "projectId": "gabssari",
+  "storageBucket": "gabssari.firebasestorage.app",
+  "messagingSenderId": "225123716024",
+  "appId": "1:225123716024:web:9960c89ab4c04862795d4d"
+};
